6eecef03fbb08a3053e49df501cbf834d21bbf06
Sanjeev <ms.saisanjeev@gmail.com>
2025-09-15 16:39:42 +0530

    add video

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,7 +8,7 @@ import DroolWeb from '../assets/DroolWeb.mp4'
 import EflyerWeb from '../assets/EflyerWeb.mp4'
 // import ludo from '../assets/ludoImage.mp4'
 // import PacMan from '../assets/PacmanMove.mp4'
-// import SolarSystem from '../assets/SolarSystem.mp4'
+import SolarSystem from '../assets/SolarSystem.mp4'
 import TodoApp from '../assets/TodoApp.mp4'
 import Restaurant from '../assets/Restaurant.mp4'
 import RestaurantWeb from '../assets/RestaurantWeb.mp4'
@@ -22,7 +22,7 @@ import EflyerPoster from '../assets/EflyerWeb.png'
 // import PacmanPoster from '../assets/PacmanMove.png'
 import RestaurantPoster from '../assets/Restaurant.png'
 import RestaurantWebPoster from '../assets/RestaurantWeb.png'
-// import SolarPoster from '../assets/SolarSystem.png'
+import SolarPoster from '../assets/SolarSystem.png'
 import TodoAppPoster from '../assets/TodoApp.png'
 
 import VideoShow from '../components/VideoShow'
@@ -73,12 +73,12 @@ const Projects = () => {
       videoName: EflyerWeb,
       posterName: EflyerPoster
     },
-    // {
-    //   title1: "Solar Planet Moving : WebPage",
-    //    title2: "Using : HTML CSS (Animation)",
-    //   videoName: SolarSystem,
-    //   posterName: SolarPoster
-    // },
+    {
+      title1: "Solar Planet Moving : WebPage",
+       title2: "Using : HTML CSS (Animation)",
+      videoName: SolarSystem,
+      posterName: SolarPoster
+    },
     //  {
     //   title1: "Car Moving : Webpage",
     //    title2: "Using : HTML CSS (Animation)",
